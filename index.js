1a029d64d2b6780b0e4e6c70f0df2eebb13c53e7
Anmisha Dash <dashanmisha@gmail.com>
2026-08-25 18:24:11 +0530

    updated the username/checked the password using patch and method-overriding

--- index.js
+++ index.js
@@ -86,7 +86,33 @@ app.get("/user/:id/edit",(req,res)=>{
 
 //Update route
 app.patch("/user/:id",(req,res)=>{
-    res.send("updated");
+
+    let {id}=req.params;
+    let {password : formPass,username : newUsername} = req.body;
+    let q = `SELECT * FROM user WHERE id ='${id}'`;
+    connection.query(q, (err, result) => {
+        if (err) {
+            console.log(err);         
+            res.send("errorr");
+            return;
+        }
+        else{
+            // res.send(result);
+            let user = result[0];
+            
+            if(formPass != user.password){
+                res.send("wrong password");
+            }
+            else{
+                let q2= `UPDATE user SET username = '${newUsername}' WHERE id ='${id}'`;
+                connection.query(q2,(err,result)=>{
+                    if (err) throw err;
+                    res.redirect("/user");
+                })
+            }
+        }
+    });
+    
 })
 
 app.listen(8080, () => {
